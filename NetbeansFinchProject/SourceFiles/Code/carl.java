dcdb15688eaca6f0e836851b596cbf719aa1fbde
Zachary Zee <Oneman2feet@gmail.com>
2011-11-29 17:05:21 -0500

    fixed

--- NetbeansFinchProject/SourceFiles/Code/carl.java
+++ NetbeansFinchProject/SourceFiles/Code/carl.java
@@ -9,15 +9,16 @@ import edu.cmu.ri.createlab.terk.robot.finch.Finch;
  * A starter file to use the Finch
  */
 
-public class FinchTemplateFile
+public class carl
    {
    public static void main(final String[] args)
       {
       // Instantiating the Finch object
       Finch myFinch = new Finch();
 
-      // Write some code here!
-
+      // What to do:
+      
+      
       // Always end your program with finch.quit()
       myFinch.quit();
       System.exit(0);
