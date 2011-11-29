60cbfd644440fb39afcd872f271dc4e37ea7535e
ASUS <prog694@gmail.com>
2011-11-29 17:28:40 -0500

    add finch polygon untested

--- NetbeansFinchProject/SourceFiles/Code/TeamLarry.java
+++ NetbeansFinchProject/SourceFiles/Code/TeamLarry.java
@@ -14,18 +14,32 @@ public class TeamLarry
    {
    public static void main(final String[] args)
       {
-      // Instantiating the Finch object
+      //nstantiating the Finch object
       Finch myFinch = new Finch();
       TeamLarry teamLarry = new TeamLarry();
       
-      teamLarry.darwin(myFinch, 5);
+     //teamLarry.darwin(myFinch, 5);
+     teamLarry.polygon(myFinch, 3);
       
       
       // Always end your program with finch.quit()
       myFinch.quit();
       System.exit(0);
       }
-   
+
+   public void polygon(Finch myFinch, int sides){
+       int speed = 190;
+       int time = 500;
+       int timeTurn = 1900/sides;
+
+       for(int i = 0; i < sides; i++){
+        myFinch.setWheelVelocities(speed, speed, time);
+        myFinch.setWheelVelocities(speed, -speed, timeTurn);
+
+       }
+    }
+   }
+   /*
    public void darwin(Finch myFinch, int loop){
         for(int c = 0; c < loop; c++){
             int speed = 255;
@@ -53,4 +67,7 @@ public void square(Finch myFinch){
     }
 }  
    }
+    *
+    */
+
 
