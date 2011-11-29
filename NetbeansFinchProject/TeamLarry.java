2d3b904ad2a61f820b0c2d303bd318267c8302e3
George Vorobyev <quaffle97@yahoo.com>
2011-11-29 16:14:31 -0500

    added derp function:  random time for each wheel at random velocity, when time is up chooses new time and velocity

--- NetbeansFinchProject/TeamLarry.java
+++ NetbeansFinchProject/TeamLarry.java
@@ -3,7 +3,7 @@ package Code;
 
 import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 import java.io.*;
-
+import java.util.*;
 /**
  * Created by:
  * Date:
@@ -15,20 +15,55 @@ public class TeamLarry {
         // Instantiating the Finch object
 
         Finch myFinch = new Finch();
-
+        
         // Write some code here!
         //myFinch.buzz(262,100000);
 
         
-        myFinch.setWheelVelocities(255,255,0);
-        while (myFinch.isObstacle()){
-            myFinch.setWheelVelocities(255,255,1000);
-        }
+        derp(10,255,myFinch);
 
 
         // Always end your program with finch.quit()
         myFinch.quit();
         System.exit(0);
     }
+    
+    public static void derp(float Time,int maxvel, Finch myFinch)
+    {
+      //  Random rand = new Random();
+        float time = Time*1000;
+        float lefttime=0;
+        float righttime=0;
+        float time1 = System.currentTimeMillis();
+        int velr;
+        int vell;
+       
+        while (time>0){
+           if (lefttime<=0){
+               lefttime=(long)(Math.random()*2000);
+               vell =(int)(Math.random()*2*maxvel-maxvel);}
+               
+           if (righttime<=0){
+            righttime=(long)(Math.random()*2000);
+            velr =(int)(Math.random()*2*maxvel-maxvel);}
+           
+           
+        
+        
+        
+                
+            myFinch.setWheelVelocities(vell,velr);
+            float difference =(System.currentTimeMillis()-time1);
+            time-=difference;
+            lefttime-=difference;
+            righttime-=difference;
+            
+        }
+        myFinch.setWheelVelocities(0,0);
+        
+        
+        
+    }
+    
 }
 
