ab29f8a2f3723907d7cb09a48d7520a9359bfea3
Zachary Zee <Oneman2feet@gmail.com>
2011-11-29 17:02:10 -0500

    sexy carl

---
+++ NetbeansFinchProject/SourceFiles/Code/carl.java
@@ -0,0 +1,26 @@
+package Code;
+// Needs a package declaration to move to another folder
+
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+/**
+ * Created by:
+ * Date:
+ * A starter file to use the Finch
+ */
+
+public class FinchTemplateFile
+   {
+   public static void main(final String[] args)
+      {
+      // Instantiating the Finch object
+      Finch myFinch = new Finch();
+
+      // Write some code here!
+
+      // Always end your program with finch.quit()
+      myFinch.quit();
+      System.exit(0);
+      }
+   }
+
