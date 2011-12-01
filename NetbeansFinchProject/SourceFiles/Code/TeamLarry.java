4343e76121b5fd47aa14f8f0687557e4e0b3dcc6
George Vorobyev <quaffle97@yahoo.com>
2011-12-01 17:30:20 -0500

    Derp V2: Added more parameters to function, made min-max time and velocity, did some optimizing, improved time calculation

--- NetbeansFinchProject/SourceFiles/Code/TeamLarry.java
+++ NetbeansFinchProject/SourceFiles/Code/TeamLarry.java
@@ -5,8 +5,8 @@ import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 import java.io.*;
 import java.util.*;
 /**
- * Created by:
- * Date:
+ * Created by: George, and not the rest of the team because they're all derps
+ * Date:12/1/11
  * A starter file to use the Finch
  */
 public class TeamLarry {
@@ -16,11 +16,7 @@ public class TeamLarry {
 
         Finch myFinch = new Finch();
         
-        // Write some code here!
-        //myFinch.buzz(262,100000);
-
-        
-        derp(10,255,myFinch);
+        derp(5,100,255,0,300,myFinch);
 
 
         // Always end your program with finch.quit()
@@ -28,45 +24,50 @@ public class TeamLarry {
         System.exit(0);
     }
     
-    public static void derp(float Time,int maxvel, Finch myFinch)
+    public static void derp(float Time,int MinVel,int MaxVel,long MinTime, long MaxTime, Finch myFinch)
     {
-      //  Random rand = new Random();
         long minTime=200;
         long maxTime=500;
+        long minVel=0;
+        long maxVel=255;
         
-        long time = (long)Time*1000;
+        minTime=MinTime;
+        maxTime=MaxTime;
+        minVel=MinVel;
+        maxVel=MaxVel;
+        
+        long time = (long)(Time*1000);
         long lefttime=0;
         long righttime=0;
         long time1 = (long)System.currentTimeMillis();
+        long timeStart = (long)System.currentTimeMillis();
         int velr=0;
         int vell=0;
+        boolean hasChanged=false;
        
-        while (time>0){
+        while (time+timeStart>(long)System.currentTimeMillis()){
            if (lefttime<=0){
                lefttime=(long)(Math.random()*(maxTime-minTime)+minTime);
-               vell =(int)(Math.random()*2*maxvel-maxvel);}
+               vell =(int)(Math.random()*(maxVel-minVel)+minVel)*(int)(Math.round(Math.random())*2-1);
+               hasChanged=true;}
                
            if (righttime<=0){
-            righttime=(long)(Math.random()*(maxTime-minTime)+minTime);
-            velr =(int)(Math.random()*2*maxvel-maxvel);}
-           
-           
-        
-        
-        
-                
-            myFinch.setWheelVelocities(vell,velr);
+               righttime=(long)(Math.random()*(maxTime-minTime)+minTime);
+               velr =(int)(Math.random()*(maxVel-minVel)+minVel)*(int)(Math.round(Math.random())*2-1);
+               hasChanged=true;}
+                  
+               
+            if(hasChanged){
+                myFinch.setWheelVelocities(vell,velr);
+                hasChanged=false;
+            }
             long difference =(long)(System.currentTimeMillis()-time1);
-            time-=difference;
             lefttime-=difference;
             righttime-=difference;
             time1=System.currentTimeMillis();
             
         }
         myFinch.setWheelVelocities(0,0);
-        
-        
-        
     }
     
 }
